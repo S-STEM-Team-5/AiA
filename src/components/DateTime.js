a97ebc3d031c357aff223def04dc70d51f8b6f6e
Kevin Kamto <kamtokevin@gmail.com>
2019-07-22 02:28:30 -0400

    style: added styling for logo & dateTime

--- src/components/DateTime.js
+++ src/components/DateTime.js
@@ -1,11 +1,19 @@
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 let date = new Date()
 
 export function Header() {
 return (
         <View>
-            <Text>{date.toTimeString}</Text>
+            <Text style={ css.a } >{date.toTimeString}</Text>
         </View>
     )
 }
+
+const css = StyleSheet.create({
+    a: {
+        textAlign: "right",
+        fontSize: "20",
+        fontWeight: "bold"
+    }
+})
--- src/components/tresorLogo.js
+++ src/components/tresorLogo.js
@@ -5,26 +5,20 @@ import {
     StyleSheet
 } from 'react-native';
 
-
-
-export default class tresorLogo extends React.Component {
-    render() {
-        return(
-            <Image source={img} />
-            )
-    }
-}
-const css = StyleSheet.create({
-    image: {
-        flex: 1,
-        justifyContent: "center",
-        alignContent: "center",
-        width: "10%",
-        height: "60%",
-        opacity: "0"
-    }
-
-})
-
 const img = require('../assets/tresor-xi.png')
 
+
+export default function TresorLogo() {
+        return(
+            <Image source={img} style={style.default} />
+            )
+    }
+
+const style = StyleSheet.create({
+    default: {
+        width: 240,
+        height: 220,
+        resizeMode: 'contain',
+        alignSelf: "center"
+    }
+})
